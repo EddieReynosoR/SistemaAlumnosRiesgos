5ae25cb550bd4c086119c33e0593b646ba17f93a
EddieReynosoR <105455890+EddieReynosoR@users.noreply.github.com>
2025-12-07 17:39:53 -0800

    Corregir archivo vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,3 @@
-/// <reference types="vitest" />
-/// <reference types="vite/client" />
-
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
@@ -8,8 +5,7 @@ export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: 'jsdom',       // para apps React
-    setupFiles: './src/setupTests.ts', // opcional, si tienes este archivo
+    environment: 'jsdom',
     coverage: {
       provider: 'v8',
       reporter: ['text', 'lcov'],
